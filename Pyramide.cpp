5d7bfd357cc85af5d1d063f0aedb257094ebba9f
tmp <tmp>
2018-02-15 23:30:04 +0100

    first commit

---
+++ Pyramide.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/*     #
+ *    ###
+ *   #####
+ *  #######
+ * #########
+ */
+
+/* gcc -Wall Pyramide.cpp -o Pyramide.exe */
+
+int main (int argc, char *argv[]){
+
+	int height = 0;
+
+	cout << "Rentrez la taille de la pyramide" << endl;
+	cin >> height;
+	cout << "La taille vaut " << height << endl;
+
+	for (int i = 0; i < height; i++) {
+		if (i == 0) {
+			for (int j = 0; j < height; j++) {
+				cout << " ";
+			}	
+			cout << "#" << endl;	
+		}
+		else {
+			for (int j = 0; j < height-i; j++) {
+				cout << " ";
+			}
+			for (int j = 0; j < (2*i+1); j++) {
+				cout << "#";
+			}	
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
